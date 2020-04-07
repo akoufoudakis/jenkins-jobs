97ceb3defefbd705fbabdc18f932411c123725e8
akoufoudakis <akoufoudakis@gmail.com>
2020-04-07 23:54:49 +0200

    Added file reader

--- deploy_jobs.groovy
+++ deploy_jobs.groovy
@@ -1,6 +1,11 @@
 def repoList = []
-repoList.add('drools')
-repoList.add('lienzo-core')
+
+configFileProvider([configFile(fileId: '9018a8b6-f91d-4da6-9007-03f68830e118', variable:'repoListFile')]) {
+  def repoFile = readFile "$repoListFile"
+  def repoList = repoFile.readLines()
+
+}
+
 for(repo in repoList) {
   def jobName = 'scan' + "${repo}"
   job(jobName) {
