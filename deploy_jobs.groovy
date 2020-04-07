9dbddde670859d5df358e3f0ba10a895e2e7aef2
akoufoudakis <akoufoudakis@gmail.com>
2020-04-08 01:01:43 +0200

    Added explicit repo list

--- deploy_jobs.groovy
+++ deploy_jobs.groovy
@@ -1,13 +1,13 @@
-def repoList = []
-
-configFileProvider([configFile(fileId: '9018a8b6-f91d-4da6-9007-03f68830e118', variable:'repoListFile')]) {
-  def repoFile = readFile "$repoListFile"
-  repoList = repoFile.readLines()
-
-}
+def repoList = ['lienzo-core',
+                'lienzo-tests',
+                'droolsjbpm-build-bootstrap',
+                'kie-soup',
+                'appformer',
+                'droolsjbpm-knowledge',
+                'drools']
 
 for(repo in repoList) {
-  def jobName = 'scan' + "${repo}"
+  def jobName = 'srcclr-scan-' + "${repo}"
   job(jobName) {
     parameters {
       stringParam('URL', '', 'Repository URL')
