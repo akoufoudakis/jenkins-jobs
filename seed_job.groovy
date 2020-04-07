60b740c33ee78832a19c0b1e5e17c6b553d22a3f
akoufoudakis <akoufoudakis@gmail.com>
2020-04-07 20:24:42 +0200

    Combined the pipeline and the job

--- seed_job.groovy
+++ seed_job.groovy
@@ -1,11 +1,38 @@
 def project = ""
 
+def repoList = []
+def prefix = 'https://www.github.com/kiegroup'
 
-matrixJob("matrix-job-${project}") {
+node('master') {
+   stage('Read file') { // for display purposes
+      repoList.add('drools')
+      repoList.add('lienzo-core')
+      repoList.add('lienzo-test')
+   }
+}
 
-axes {
-     labelExpression("label-exp","master")
- }
+def branches = [:]
+
+for (repo in repoList) {
+    def branchName = 'Source Clear ' + "${repo}"
+
+    branches[branchName] = {
+        node ('master'){
+            stage(branchName) {
+                def url = "${prefix}" + "${repo}"
+                build job: "matrix-job-${repo}", propagate: false,
+                  parameters: [
+                    [$class: 'StringParameterValue', name: 'project', value: "${repo}"],
+                    [$class: 'StringParameterValue', name: 'URL', value: "${url}"]
+                  ]
+            }
+        }
+    }
+}
+
+parallel branches
+
+job("matrix-job-${project}") {
 
   parameters {
     stringParam('project', '', 'The name of the project to be scanned')
@@ -14,7 +41,7 @@ axes {
 
 
   steps {
-    shell('echo "${URL}"')
+    shell('echo "${URL}" + '/' + "${project}"')
   }
 
 }
