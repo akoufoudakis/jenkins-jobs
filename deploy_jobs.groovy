6e8419792a54264ba2a7e069468fbeac45f7a2ae
akoufoudakis <akoufoudakis@gmail.com>
2020-04-09 20:55:48 +0200

    Added mscm

--- deploy_jobs.groovy
+++ deploy_jobs.groovy
@@ -1,20 +1,6 @@
 job("generateJobs") {
 
-  scm {
-    git {
-      remote {
-        name('origin')
-        url('https://github.com/akoufoudakis/repo_bootstrap')
-      }
-      branch('master')
-      extensions {
-        cleanAfterCheckout()
-        relativeTargetDirectory('repo_bootstrap')
-      }
-    }
-  }
-
-  def repoFilePath = "./repo_bootstrap/repo-list.txt"
+  def repoFilePath = "repo_bootstrap/repo-list.txt"
   def repoFile = readFileFromWorkspace(repoFilePath);
   def repoList = repoFile.split()
   for(repo in repoList) {
