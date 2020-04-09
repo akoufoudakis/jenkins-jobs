b77335ccc0e266297a6eec14eac90aab94dae534
akoufoudakis <akoufoudakis@gmail.com>
2020-04-09 19:57:39 +0200

    Put correct URL

--- deploy_jobs.groovy
+++ deploy_jobs.groovy
@@ -4,13 +4,13 @@ job("generateJobs") {
     git {
       remote {
         name('origin')
-        url('https://github.com/project-ncl/sourceclear-invoker')
+        url('https://github.com/akoufoudakis/repo_bootstrap')
       }
       branch('master')
     }
   }
-  
-  def repoFilePath = "repo-list.txt"
+
+  def repoFilePath = "./repo-list.txt"
   def repoFile = readFileFromWorkspace(repoFilePath);
   def repoList = repoFile.split()
   for(repo in repoList) {
