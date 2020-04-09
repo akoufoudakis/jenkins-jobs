2b38ed8e34029011fbdd776ec27ea8300978ff32
akoufoudakis <akoufoudakis@gmail.com>
2020-04-09 20:58:08 +0200

    Removed outer job

--- deploy_jobs.groovy
+++ deploy_jobs.groovy
@@ -1,19 +1,16 @@
-job("generateJobs") {
-
-  def repoFilePath = "repo_bootstrap/repo-list.txt"
-  def repoFile = readFileFromWorkspace(repoFilePath);
-  def repoList = repoFile.split()
-  for(repo in repoList) {
-    def jobName = 'srcclr/scan-' + "${repo}"
-    job(jobName) {
-      parameters {
-        stringParam('URL', '', 'Repository URL')
-      }
-
-      steps {
-        shell('echo "${URL}"')
-      }
-
+def repoFilePath = "repo_bootstrap/repo-list.txt"
+def repoFile = readFileFromWorkspace(repoFilePath);
+def repoList = repoFile.split()
+for(repo in repoList) {
+  def jobName = 'srcclr/scan-' + "${repo}"
+  job(jobName) {
+    parameters {
+      stringParam('URL', '', 'Repository URL')
     }
+
+    steps {
+      shell('echo "${URL}"')
+    }
+
   }
 }
