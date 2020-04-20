351747dff259ab8fad4be14b572e9f238682191f
akoufoudakis <akoufoudakis@gmail.com>
2020-04-20 17:00:11 +0200

    Added more parameters

--- scan_seed_job.groovy
+++ scan_seed_job.groovy
@@ -1,15 +1,18 @@
 job('srcclr_scan_seed_job') {
   description('Scan job, which generates scanning jobs for upstream projects')
   parameters {
-            stringParam('REPO_FILE_URL','https://raw.githubusercontent.com/akoufoudakis/droolsjbpm-build-bootstrap/BXMSPROD-533/script/repository-list.txt','URL of the rpository-list.txt file')
+    stringParam('REPO_FILE_URL','https://raw.githubusercontent.com/akoufoudakis/droolsjbpm-build-bootstrap/BXMSPROD-533/script/repository-list.txt','URL of the rpository-list.txt file')
+    /* stringParam('KIE_JENKINS_SCRIPTS_REPO', 'https://github.com/akoufoudakis/kie-jenkins-scripts.git', '') */
+    stringParam('KIE_JENKINS_SCRIPTS_REPO', 'https://github.com/akoufoudakis/jenkins-jobs', '')
+    stringParam('KIE_JENKINS_SCRIPTS_BRANCH', 'master', '')
   }
   scm {
     git {
       remote {
         name('origin')
-        url('https://github.com/akoufoudakis/jenkins-jobs')
+        url("${KIE_JENKINS_SCRIPTS_REPO}")
       }
-      branch('master')
+      branch("${KIE_JENKINS_SCRIPTS_BRANCH}")
     }
   }
 
