4adc95da66759aa54dedf93c7ce1271eb3c1545f
akoufoudakis <akoufoudakis@gmail.com>
2020-04-21 00:46:35 +0200

    Added path to jobs

--- scan_job.groovy
+++ scan_job.groovy
@@ -1,7 +1,7 @@
 repoFile = readFileFromWorkspace('repository-list.txt')
 def repos = repoFile.readLines()
 for(repo in repos) {
-  def jobName = "${repo}"
+  def jobName = "${JOB_PATH}/${repo}"
   job(jobName) {
     description('New job')
   }
